85681dc60633061489caaa545378bc17d153f130
jangabriel <jangabriel@cppengineer.com>
2021-11-04 06:01:19 +0000

    Started with convolution strategy. Need to add Unit Test.

--- include/video-detect/img/conv.h
+++ include/video-detect/img/conv.h
@@ -2,6 +2,7 @@
 #ifndef VIDEO_DETECT_INCLUDE_VIDEO_DETECT_IMG_CONV_H_
 #define VIDEO_DETECT_INCLUDE_VIDEO_DETECT_IMG_CONV_H_
 
+#include <iostream>
 #include <vector>
 
 #include <opencv2/core/mat.hpp>
@@ -14,13 +15,39 @@ namespace img {
 
 /**
  * This function uses convulution between the provided Matrix and the Kernel
- * @param mat       a 2D matrix
- * @param kernel    a Kernel to use in the Convolution
+ * @param mat       a 2D matrix with 3 channels depth e.g. a RGB image
+ * @param kernel    an integer Kernel to use in the Convolution
  * @return          the resulting 2D matrix
- */ 
-static cv::Mat ConvCvMatKernel(const cv::Mat &mat, const Kernel &kernel) {
-    cv::Mat result = mat.clone();
-    return result;
+ */
+static cv::Mat ConvCvMat3bKernel(const cv::Mat &mat,
+                                 const Kernel<int> &kernel) {
+  cv::Mat result = mat.clone();
+
+  // Two D - traverse the matrix
+  for (int row = 0; row < mat.rows; row++) {
+    for (int col = 0; col < mat.cols; col++) {
+
+      // Now travers the perform the matrix multiplication
+      uint8_t r{},g{},b{};
+
+      for (int y = -kernel.GetRowCount() / 2; y < kernel.GetRowCount() / 2;
+           y++) {
+        for (int x = -kernel.GetColCount() / 2; x < kernel.GetColCount() / 2;
+             x++) {
+          // Multiply the matrix value with the corresponding kernel value       
+          r += (uint8_t)result.at<cv::Vec3b>(row + y, col + x)[0];
+          g += (uint8_t)result.at<cv::Vec3b>(row + y, col + x)[1];
+          b += (uint8_t)result.at<cv::Vec3b>(row + y, col + x)[2];
+        }
+      }
+      // Set the new pixel value  
+      result.at<cv::Vec3b>(row, col, 0) = r / kernel.GetSumOfContents();
+      result.at<cv::Vec3b>(row, col, 1) = g / kernel.GetSumOfContents();
+      result.at<cv::Vec3b>(row, col, 2) = b / kernel.GetSumOfContents();
+    }
+  }
+
+  return result;
 }
 
 } // namespace img
--- include/video-detect/img/gaussian_blur.h
+++ include/video-detect/img/gaussian_blur.h
@@ -8,6 +8,7 @@
 #include <opencv2/highgui.hpp>
 
 #include "video-detect/img/kernel.h"
+#include "video-detect/img/conv.h"
 #include "video-detect/util/chainable_object_receiver.h"
 
 namespace video_detect {
@@ -24,8 +25,8 @@ public:
 
   void Accept(cv::Mat img) override {
 
-    // Create a new Mat class for the blurred image
-    cv::Mat img_blur = BlurImage(img);
+    // Create a new Mat class from the blurred image
+    cv::Mat img_blur = ConvCvMat3bKernel(img, kernel_3x3_);  
 
     // Save the images
     if (export_images_) {
@@ -49,25 +50,14 @@ private:
   const bool export_images_;
 
   // Gaussian kernel 3x3
-  const Kernel<int> kernel_3x3{{{1, 2, 1}, {2, 4, 2}, {1, 2, 1}}};
+  const Kernel<int> kernel_3x3_{{{1, 2, 1}, {2, 4, 2}, {1, 2, 1}}};
 
   // Gaussian kernel 5x5
-  const Kernel<int> kernel_5x5{{{1, 4, 7, 4, 1},
+  const Kernel<int> kernel_5x5_{{{1, 4, 7, 4, 1},
                                 {4, 16, 26, 16, 4},
                                 {7, 26, 41, 26, 7},
                                 {4, 16, 26, 16, 4},
                                 {1, 4, 7, 4, 1}}};
-
-  cv::Mat BlurImage(const cv::Mat &img) {
-    cv::Mat img_blur = img.clone();
-
-    // Perform Kernel Convulotion here
-    // TODO - CONTINUE HERE
-    int cols = img.cols;
-    int rows = img.rows;
-
-    return img_blur;
-  }
 };
 
 } // namespace img
